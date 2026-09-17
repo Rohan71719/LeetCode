48af34fb4fd83074123bb5f30d188184bbd62719
ROHAN SEN <149294499+Rohan71719@users.noreply.github.com>
2026-09-17 19:20:58 +0530

    0 ms (100%) 9.7 MB (48.41%) 09-17-2026 0242-valid-anagram Easy .cpp Hash Table

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        vector<int> freq(26,0);
+        if(s.length()!=t.length())  return false;
+
+        for(char ch:s)
+            freq[ch-'a']++;
+        for(char ch:t)
+            freq[ch-'a']--;
+
+        for(int count:freq)
+        {
+            if(count!=0)
+                return false;
+        }          
+            return true;
+    }
+};

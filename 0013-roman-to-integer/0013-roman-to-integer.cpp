1d1f5d5fec9c9e2658d6308c21418a45c3baa501
ROHAN SEN <149294499+Rohan71719@users.noreply.github.com>
2026-09-18 19:16:17 +0530

    0 ms (100%) 10 MB (94.02%) 09-18-2026 0013-roman-to-integer Easy .cpp Hash Table

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int num(char c)
+{
+    if(c=='I')
+        return 1;
+    if(c=='V')
+        return 5;    
+   if(c=='X')
+        return 10;
+    if(c=='L')
+        return 50;    
+    if(c=='C')
+        return 100;
+    if(c=='D')
+        return 500;
+    else
+        return 1000;
+}        
+
+    int romanToInt(string s) 
+    {
+        int sum=0, index=0;
+        while(index<s.size()-1)
+        {
+            if(num(s[index])<num(s[index+1]))
+                {
+                    sum=sum-num(s[index]);
+                    index++;
+                }    
+            else{
+                sum+=num(s[index]);
+                index++;
+                }       
+        }
+            sum+=num(s[index]);
+            return sum;
+    }
+};
